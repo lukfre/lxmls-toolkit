82585d91be05b800e04d8b76e9f824a2f9d32135
lukfre <balthier@tuta.io>
2024-07-11 16:17:28 +0100

    exercise 1.1

--- lxmls/classifiers/multinomial_naive_bayes.py
+++ lxmls/classifiers/multinomial_naive_bayes.py
@@ -22,8 +22,10 @@ class MultinomialNaiveBayes(lc.LinearClassifier):
 
         # classes = a list of possible classes
         classes = np.unique(y)
+        print("classes", classes)
         # n_classes = no. of classes
         n_classes = np.unique(y).shape[0]
+        print("n_classes", n_classes)
 
         # initialization of the prior and likelihood variables
         prior = np.zeros(n_classes)
@@ -42,12 +44,36 @@ class MultinomialNaiveBayes(lc.LinearClassifier):
         # ----------
         # Solution to Exercise 1
 
-        raise NotImplementedError("Complete Exercise 1")
+        # axis 0: doc id
+        # axis 1: bag-of-words -> value at index i is the occurrence of word i
+        print("x", x.shape)
+        print("y", y.shape)
+        neg_docs_ids, _ = np.nonzero(y == 0)
+        pos_docs_ids, _ = np.nonzero(y == 1)
+
+        prior[0] = len(neg_docs_ids) / n_docs
+        prior[1] = len(pos_docs_ids) / n_docs
+        print("prior", prior)
+
+        # compute the frequency of words across the documents (axis 0)
+        neg_docs_words_frequency = x[neg_docs_ids, :].sum(0)
+        pos_docs_words_frequency = x[pos_docs_ids, :].sum(0)
+
+        # the likelihood is the relative frequence of the words in the docs belonging to a given class
+        #   -> just divide by the total number of words in the documents in that class
+        likelihood[:, 0] = (1 + neg_docs_words_frequency) / (
+            x[neg_docs_ids, :].sum() + n_words
+        )
+        likelihood[:, 1] = (1 + pos_docs_words_frequency) / (
+            x[pos_docs_ids, :].sum() + n_words
+        )
+
+        # raise NotImplementedError("Complete Exercise 1")
 
         # End solution to Exercise 1
         # ----------
 
-        params = np.zeros((n_words+1, n_classes))
+        params = np.zeros((n_words + 1, n_classes))
         for i in range(n_classes):
             params[0, i] = np.log(prior[i])
             params[1:, i] = np.nan_to_num(np.log(likelihood[:, i]))
